87c5029ad297e652db3fdd63fde2c78da5747537
Ivan Prakapets <ivanprokopets1999@gmail.com>
2020-11-29 19:12:43 +0100

    add users and statusApp & statusTask

--- db/start_scripts/seed.sql
+++ db/start_scripts/seed.sql
@@ -1,9 +1,9 @@
-INSERT INTO AppStore (name, ranking, dateUpdate, description, timesUsed)
-VALUES ('Kalkulator', 5, '2020-10-10', 'Kalkulator jak to kalkulator', 69);
-INSERT INTO AppStore (name, ranking, dateUpdate, description, timesUsed)
-VALUES ('Aplikacja do rozpoznawania gestów', 4, '2020-11-11', 'Aplikacja rozpoznaje gesty na obrazie/filmie', 440);
-INSERT INTO AppStore (name, ranking, dateUpdate, description, timesUsed)
-VALUES ('Aplikacja do prognozowania kursów walut', 1, '2020-11-13', 'Aplikacja prognozuje kursy walut takich jak dolar czy euro', 1000);
+INSERT INTO AppStore (name, ranking, dateUpdate, description, timesUsed, statusApp, IdUser)
+VALUES ('Kalkulator', 5, '2020-10-10', 'Kalkulator jak to kalkulator', 69, 'available', 1);
+INSERT INTO AppStore (name, ranking, dateUpdate, description, timesUsed, statusApp, IdUser)
+VALUES ('Aplikacja do rozpoznawania gestów', 4, '2020-11-11', 'Aplikacja rozpoznaje gesty na obrazie/filmie', 440, 'available', 2);
+INSERT INTO AppStore (name, ranking, dateUpdate, description, timesUsed, statusApp, IdUser)
+VALUES ('Aplikacja do prognozowania kursów walut', 1, '2020-11-13', 'Aplikacja prognozuje kursy walut takich jak dolar czy euro', 1000, 'to_delete', 3);
 
 INSERT INTO DataShelf (dataSetShelf, name, dataType, accessType, value, IdApp)
 VALUES ('data self shelf 1', 'Data Shelf nr 1', 'int', 'full access', 5, 1);
@@ -19,12 +19,12 @@ VALUES (4.0, 'Całkiem niezłe narzędzie', '2020-11-12', 1, 2);
 INSERT INTO Rating (value, comm, dateUpdate, IdUser, IdApp)
 VALUES (1.0, 'DNO I 5 METROW MULU', '2020-11-13', 1, 3);
 
-INSERT INTO Cockpit (name, version, dateStart, dateEnd, consumedCredits, reservedCredits, status, priority, private, clusterAllocation, IdApp)
-VALUES ('Task nr 1', 1, '2020-11-12', '2020-11-13', '50', '40', 'done', 1, 'no private', 'cluster yes', 1);
-INSERT INTO Cockpit (name, version, dateStart, dateEnd, consumedCredits, reservedCredits, status, priority, private, clusterAllocation, IdApp)
-VALUES ('Task nr 2', 2, '2020-11-12', '2020-11-13', '90', '30', 'done', 2, 'private', 'cluster yes', 2);
-INSERT INTO Cockpit (name, version, dateStart, dateEnd, consumedCredits, reservedCredits, status, priority, private, clusterAllocation, IdApp)
-VALUES ('Task nr 3', 3, '2020-11-12', '2020-11-13', '30', '20', 'done', 4, 'private', 'cluster yes', 3);
+INSERT INTO Cockpit (name, version, dateStart, dateEnd, consumedCredits, reservedCredits, status, priority, private, clusterAllocation, IdApp, IdUser)
+VALUES ('Task nr 1', 1, '2020-11-12', '2020-11-13', '50', '40', 'created', 1, 'no private', 'cluster yes', 1, 1);
+INSERT INTO Cockpit (name, version, dateStart, dateEnd, consumedCredits, reservedCredits, status, priority, private, clusterAllocation, IdApp, IdUser)
+VALUES ('Task nr 2', 2, '2020-11-12', '2020-11-13', '90', '30', 'ongoing', 2, 'private', 'cluster yes', 2, 1);
+INSERT INTO Cockpit (name, version, dateStart, dateEnd, consumedCredits, reservedCredits, status, priority, private, clusterAllocation, IdApp, IdUser)
+VALUES ('Task nr 3', 3, '2020-11-12', '2020-11-13', '30', '20', 'finished', 4, 'private', 'cluster yes', 3, 1);
 
 INSERT INTO Image (filename, img, IdApp) 
 VALUES ('img1.jpg', bytea('/images/img1.jpg'), 1);
@@ -32,3 +32,10 @@ INSERT INTO Image (filename, img, IdApp)
 VALUES ('img2.jpg', bytea('/images/img2.jpg'), 2);
 INSERT INTO Image (filename, img, IdApp) 
 VALUES ('img3.jpg', bytea('/images/img3.jpg'), 3);
+
+INSERT INTO Users (login, password, status) 
+VALUES ('John1', 'test!.12a', 'OK');
+INSERT INTO Users (login, password, status) 
+VALUES ('Antoni2', '1teast!.12', 'OK');
+INSERT INTO Users (login, password, status) 
+VALUES ('Artur3', 'test!.12', 'OK');
